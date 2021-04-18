7675051977aec45cdf3ddefaaab9657c22e22f71
Рамис Билалов <ramiska81@gmail.com>
2021-04-18 22:24:37 +0300

    Домашняя работа к 4-му уроку

--- java-spring-boot-lesson-four/src/main/java/com/bilalov/javaspringbootlessonfour/configuration/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package com.bilalov.javaspringbootlessonfour.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("com.bilalov.javaspringbootlessonfour")
-public class AppConfig {
-}
