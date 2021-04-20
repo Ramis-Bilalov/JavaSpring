e6e46e5c6c033de20a7d317e6fb739195a91744f
Ramis Bialov <ramiska81@gmail.com>
2021-04-20 22:38:55 +0300

    Домашняя работа к 5-му уроку

--- java-spring-boot-lesson-four/src/main/java/com/bilalov/javaspringbootlessonfour/controllers/ProductController.java
+++ java-spring-boot-lesson-four/src/main/java/com/bilalov/javaspringbootlessonfour/controllers/ProductController.java
@@ -10,45 +10,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/product")
+//@Controller
+//@RequestMapping("/product")
 public class ProductController {
 
-    @Autowired
-    private ProductService productService;
-//    private Product product;
-
-    @GetMapping
-    public String indexPage(Model model) {
-        model.addAttribute("products", productService.getAllProduct());
-        return "product-views/index";
-    }
-
-    @GetMapping("/{id}")
-    public String editProduct(@PathVariable(value = "id") Long id, Model model ) {
-        model.addAttribute("product", productService.getById(id));
-        return "product-views/product_form";
-    }
-
-    @PostMapping("/product_update")
-    public String updateProduct(Product product) {
-        if(product.getId() == null) {
-            productService.add(product);
-        } else {
-            productService.update(product);
-        }
-        return "redirect:/product";
-    }
-
-    @GetMapping("/new")
-    public String newProduct(Product product) {
-        productService.add(product);
-        return "product-views/product_form"; // не заполненная!!!
-    }
-
-    @GetMapping("/delete/{id}")
-    public String removeProduct(@PathVariable(value = "id") Long id) {
-        productService.remove(id);
-        return "redirect:/product"; // не заполненная!!!
-    }
+//    @Autowired
+//    private ProductService productService;
+////    private Product product;
+//
+//    @GetMapping
+//    public String indexPage(Model model) {
+//        model.addAttribute("products", productService.getAllProduct());
+//        return "product-views/index";
+//    }
+//
+//    @GetMapping("/{id}")
+//    public String editProduct(@PathVariable(value = "id") Long id, Model model ) {
+//        model.addAttribute("product", productService.getById(id));
+//        return "product-views/product_form";
+//    }
+//
+//    @PostMapping("/product_update")
+//    public String updateProduct(Product product) {
+//        if(product.getId() == null) {
+//            productService.add(product);
+//        } else {
+//            productService.update(product);
+//        }
+//        return "redirect:/product";
+//    }
+//
+//    @GetMapping("/new")
+//    public String newProduct(Product product) {
+//        productService.add(product);
+//        return "product-views/product_form"; // не заполненная!!!
+//    }
+//
+//    @GetMapping("/delete/{id}")
+//    public String removeProduct(@PathVariable(value = "id") Long id) {
+//        productService.remove(id);
+//        return "redirect:/product"; // не заполненная!!!
+//    }
 }
