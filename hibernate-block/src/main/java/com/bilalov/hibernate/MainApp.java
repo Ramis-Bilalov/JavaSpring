6339c68adb7c5c57116ba4d4468811c88ab702aa
Ramis Bialov <ramiska81@gmail.com>
2021-04-23 13:40:03 +0300

    Домашняя работа к 5-му уроку

--- hibernate-block/src/main/java/com/bilalov/hibernate/MainApp.java
+++ hibernate-block/src/main/java/com/bilalov/hibernate/MainApp.java
@@ -6,7 +6,6 @@ import com.bilalov.hibernate.entities.Product;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
