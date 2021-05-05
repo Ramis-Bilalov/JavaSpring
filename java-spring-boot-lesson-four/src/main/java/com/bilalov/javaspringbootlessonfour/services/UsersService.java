41c6ee44b420a5800cc8ab897c110fe3960f96ca
Ramis Bialov <ramiska81@gmail.com>
2021-05-05 16:41:12 +0300

    Классная работа к 9-му уроку

---
+++ java-spring-boot-lesson-four/src/main/java/com/bilalov/javaspringbootlessonfour/services/UsersService.java
@@ -0,0 +1,42 @@
+package com.bilalov.javaspringbootlessonfour.services;
+
+
+import com.bilalov.javaspringbootlessonfour.entities.User;
+import com.bilalov.javaspringbootlessonfour.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UsersService {
+
+    private UserRepository repository;
+
+    @Autowired
+    public void setRepository(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public User createOrUpdate(User user) {
+        return repository.save(user);
+    }
+
+    @Transactional
+    public Optional<User> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Transactional
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+}
